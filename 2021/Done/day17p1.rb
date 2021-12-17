6fea5f2343b7e5fcca9e647dd0c4b284f53b340c
Rejna <rafal.hejna@gmail.com>
2021-12-17 12:15:45 +0100

    2021 day 17 tasks solutions

---
+++ 2021/Done/day17p1.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+# Solution to Advent of Code 2021 Day 17 Part 1
+# https://adventofcode.com/2021/day/17
+# Answer is: 7381
+
+# input = 'target area: x=20..30, y=-10..-5'.gsub('target area: ', '').split(', ')
+input = File.readlines('day17.input').map(&:strip)[0].gsub('target area: ', '').split(', ')
+
+area_x = input[0].gsub('x=', '').split('..').map(&:to_i)
+area_y = input[1].gsub('y=', '').split('..').map(&:to_i)
+
+x_min = area_x[0]
+x_max = area_x[1]
+y_min = area_y[0]
+y_max = area_y[1]
+
+start_v_x = 1
+start_v_y = y_min
+
+max_height = -999_999_999
+
+while start_v_x <= x_max
+  start_v_y = y_min
+  while start_v_y <= -y_min
+    x = 0
+    y = 0
+    v_x = start_v_x
+    v_y = start_v_y
+    local_max_height = -999_999_999
+    steps = 0
+
+    loop do
+      x += v_x
+      y += v_y
+      steps += 1
+      local_max_height = y if y > local_max_height
+
+      break if steps > 250
+
+      if x >= x_min && x <= x_max && y >= y_min && y <= y_max
+        if local_max_height > max_height
+          max_height = local_max_height
+          # puts "#{start_v_x},#{start_v_y} #{max_height} #{steps}"
+        end
+        break
+      end
+
+      v_x = if v_x.positive?
+              v_x - 1
+            elsif v_x.negative?
+              v_x + 1
+            else
+              v_x
+            end
+      v_y -= 1
+    end
+    start_v_y += 1
+  end
+  start_v_x += 1
+end
+
+puts max_height
---
+++ 2021/Done/day17p2.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+# Solution to Advent of Code 2021 Day 17 Part 2
+# https://adventofcode.com/2021/day/17#part2
+# Answer is: 3019
+
+# input = 'target area: x=20..30, y=-10..-5'.gsub('target area: ', '').split(', ')
+input = File.readlines('day17.input').map(&:strip)[0].gsub('target area: ', '').split(', ')
+
+area_x = input[0].gsub('x=', '').split('..').map(&:to_i)
+area_y = input[1].gsub('y=', '').split('..').map(&:to_i)
+
+x_min = area_x[0]
+x_max = area_x[1]
+y_min = area_y[0]
+y_max = area_y[1]
+
+start_v_x = 1
+start_v_y = y_min
+
+# speeds = []
+speeds = {}
+
+while start_v_x <= x_max
+  start_v_y = y_min
+  while start_v_y <= -y_min
+    x = 0
+    y = 0
+    v_x = start_v_x
+    v_y = start_v_y
+    steps = 0
+
+    loop do
+      x += v_x
+      y += v_y
+      steps += 1
+
+      break if steps > 250
+
+      if x >= x_min && x <= x_max && y >= y_min && y <= y_max
+        # speeds << [start_v_x, start_v_y] unless speeds.include?([start_v_x, start_v_y])
+        speeds[[start_v_x, start_v_y]] = true unless speeds.key?([start_v_x, start_v_y])
+        break
+      end
+
+      v_x = if v_x.positive?
+              v_x - 1
+            elsif v_x.negative?
+              v_x + 1
+            else
+              v_x
+            end
+      v_y -= 1
+    end
+    start_v_y += 1
+  end
+  start_v_x += 1
+end
+
+puts speeds.length
