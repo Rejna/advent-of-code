316d1314c4f57ace22e867f8d03e835af26236e2
Rejna <rafal.hejna@gmail.com>
2022-01-04 01:15:02 +0100

    2017 day 6 tasks solutions

---
+++ 2017/3_Done/day06p1.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# Solution to Advent of Code 2017 Day 6 Part 1
+# https://adventofcode.com/2017/day/6
+# Answer is: 6681
+
+input = File.readlines('../1_Input/day06.input').map(&:strip)[0].split(/\t/).map(&:to_i)
+# input = File.readlines('../1_Input/day06test.input').map(&:strip)[0].split(/\t/).map(&:to_i)
+
+counter = 0
+configurations = [input.join(',')]
+
+loop do
+  max = input.max
+  min_index = input.find_index(max)
+  input[min_index] = 0
+
+  max.times.each do |k|
+    input[(min_index + k + 1) % input.length] += 1
+  end
+
+  counter += 1
+
+  break if configurations.include?(input.join(','))
+
+  configurations << input.join(',')
+end
+
+puts counter
